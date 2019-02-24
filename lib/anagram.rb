63a3b8522e63dc45626214e1dbf511c25a919325
Tyler Hunt <tyhunt04@gmail.com>
2019-02-24 01:45:10 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,20 +1,16 @@
 # Your code goes here!
 class Anagram
   attr_accessor :word
-  ANAGRAM =
 
   def initialize(word)
     @word = word
   end
 
   def match(array)
-    anagrams = []
-    array.collect do |match|
-      if match.split("").sort == @word.split("").sort
-        anagrams << match
+    array.select do |match|
+       match.split("").sort == @word.split("").sort
       end
     end
-    anagrams
   end
 
 end
