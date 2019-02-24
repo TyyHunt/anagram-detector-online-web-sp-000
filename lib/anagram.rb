40c8f9d12ee4694eaf19f68bcf30489db57f2505
Tyler Hunt <tyhunt04@gmail.com>
2019-02-24 01:40:56 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+  ANAGRAM =
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    winners = []
+    individual_words = array.split(",")
+    individual_words.collect do |match|
+      if match.split("").sort == @word.split("").sort
+        winners << match
+      end
+    end
+    match
+  end
+
+end
